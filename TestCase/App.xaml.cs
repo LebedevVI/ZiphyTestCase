6ec0c6ee1f5a4af4fbe7c89b000dea902ce68337
Василий <@DESKTOP-BF9RVJC>
2022-06-05 11:40:15 +0300

    Добавьте файлы проекта.

---
+++ TestCase/App.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+
+using System.Threading;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace TestCase
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            _mWindow = new MainWindow();
+            _mWindow.Activate();
+            if (!MutexCheck())
+            {
+                if (_mWindow.Content is FrameworkElement fe)
+                {
+                    fe.Loaded += (ss, ee) => MultiAppError();
+                }
+
+            }
+        }
+
+        static Mutex Mutex;
+        static bool MutexCheck()
+        {
+            bool isNew;
+            Mutex = new Mutex(true, "TestCaseZiphy", out isNew);
+            return isNew;
+        }
+
+        protected async void MultiAppError()
+        {
+            ContentDialog multiAppDialog = new ContentDialog
+            {
+                Title = "Программа уже запущена!",
+                Content = "Программа уже запущена! Закройте и попробуйте вновь",
+                CloseButtonText = "Ok"
+            };
+            multiAppDialog.XamlRoot = _mWindow.Content.XamlRoot;
+            ContentDialogResult result = await multiAppDialog.ShowAsync();
+            Application.Current.Exit();
+        }
+
+
+        private Window _mWindow;
+    }
+}
---
+++ TestCase/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace TestCase
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                OnGoButtonClick(sender, e);
+            }
+        }
+
+        private async void OnGoButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Browser.Source = MakeUri(Source.Text);
+                Browser.GoForward();
+            }
+            catch
+            {
+                ContentDialog invalideSourceDialog = new ContentDialog
+                {
+                    Title = "Некорректный адрес",
+                    Content = "Закройте и попробуйте вновь",
+                    CloseButtonText = "Ok"
+                };
+                invalideSourceDialog.XamlRoot = this.Content.XamlRoot;
+                ContentDialogResult result = await invalideSourceDialog.ShowAsync();
+            }
+        }
+
+        private Uri MakeUri(string source)
+        {
+            if (!source.Contains("http://") && !source.Contains("https://"))
+            {
+                source = "https://" + source;
+            }
+            var result = new Uri(source);
+            Source.Text = source;
+            return result;
+        }
+    }
+}
